4d2c91dddeb58746267e2683f9a102c52f35960c
DenisValuev <valuev.denis91@mail.ru>
2023-12-05 16:59:03 +0300

    Mod5-branch1: Задание 5.1 Создание кортежа User

--- Mod5/Program.cs
+++ Mod5/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Заданеи 5.1
+            (string Name, string[] Dishes) User;
+            Console.WriteLine("Введите Ваше имя: ");
+            User.Name = Console.ReadLine();
+            User.Dishes = new string[5];
+            for(int i = 0; i < User.Dishes.Length; i++) 
+            {
+                Console.WriteLine("Введите любимое блюдо номер {0}",i + 1);
+                User.Dishes[i] = Console.ReadLine();
+            }
         }
     }
 }
