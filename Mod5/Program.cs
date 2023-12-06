2ac7d89a2f7acafecf946644dc99dcc68e85dc5b
DenisValuev <valuev.denis91@mail.ru>
2023-12-06 11:48:58 +0300

    Mod5-branch1: Задание 5.1.6 Ввод массива с клавиатуры, сортировка массива, вывод на экран

--- Mod5/Program.cs
+++ Mod5/Program.cs
@@ -31,14 +31,53 @@
             }
             return color;
         }
-        // Ввывод массива на консоль
-        static void ShowArray(string[] array)
+        // Ввывод массива строк на консоль
+        static void ShowArrayString(string[] array)
         {
             Console.WriteLine("Массив");
             for (int i = 0; i < array.Length; i++)
             {
                 Console.Write(array[i] + " ");
             }
+        }
+        // Ввывод массива строк на консоль
+        static void ShowArrayInt(int[] array)
+        {
+            Console.WriteLine("Массив");
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+        //Задание 5.1.6 Ввод массима с клавиатуры
+        static int[] GetArrayFromConsole()
+        {
+            var result = new int[5];
+
+            for (int i = 0; i < result.Length; i++)
+            {
+                Console.WriteLine("Введите элемент массива номер {0}", i + 1);
+                result[i] = int.Parse(Console.ReadLine());
+            }
+            return result;
+        }
+        //Сорировка массива
+        static int[] SortArray(int[] array) 
+        {
+            int temp;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
         }
         static void Main(string[] args)
         {
@@ -54,15 +93,26 @@
                 User.Dishes[i] = Console.ReadLine();
             }
             */
-            //Задание 5.1.5
+            //Задание 5.1.5 Ввод цвета с консоли
+            /*
             var favcolors = new string[3];
             for (int i = 0; i < favcolors.Length; i++)
             {
                 favcolors[i] = ShowColor();
             }
-
+            
             // Вывод на консоль массив цветов
-            ShowArray(favcolors);
+            ShowArrayString(favcolors);
+            */
+            //Задание 5.1.6 Ввод элементов массива с клавиатуры, сортировка, вывод на экран
+            var array = new int[5];
+            //Ввод массива
+            array = GetArrayFromConsole();
+            //Сортировка массива
+            Console.WriteLine("Массив отсортирован по возрастанию");
+            array = SortArray(array);
+            //Вывод массива чисел
+            ShowArrayInt(array);
         }
     }
 }
